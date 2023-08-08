1cbe66aecf2f64cb263f4e0c634716118244d948
pabloduran <pdrm56@gmail.com>
2023-08-08 14:02:00 +0200

    Version 1.0.0

---
+++ Pacific_Impact_Sahelian_Rainfall.py
@@ -0,0 +1,45 @@
+from spy4cast import Dataset, Region, Month
+from spy4cast.spy4cast import Preprocess, MCA, Crossvalidation
+
+# Pacific impact on Sahelian rainfall
+
+predictor = Dataset('HadISST_sst-1970_2020.nc', 'datasets').open('sst').slice(
+    Region(lat0=-30, latf=30,
+           lon0=-200, lonf=-60,
+           month0=Month.APR, monthf=Month.JUN,
+           year0=1976, yearf=2000),
+)
+
+predictand = Dataset('cru_ts4.06.1950.2010.nc', 'datasets').open('pre').slice(
+    Region(lat0=0, latf=25,
+           lon0=-20, lonf=20,
+           month0=Month.JUL, monthf=Month.SEP,
+           year0=1976, yearf=2000),
+)
+#  There is a lag of 3 months (from April to July)
+
+## METHODOLOGY
+
+# First step. Preprocess variables: anomaly and reshaping
+predictor_preprocessed = Preprocess(predictor)
+predictor_preprocessed.save('y_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+# Save matrices as .npy for fast loading. To load use:
+# predictor_preprocessed = Preprocess.load('y_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+predictand_preprocessed = Preprocess(predictand)
+predictand_preprocessed.save('z_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+# predictand_preprocessed = Preprocess.load('z_', 'data/-Pacific_Impact_Sahelian_Rainfall')
+
+# Second step. MCA: expansion coefficients and correlation and regression maps
+nm = 3
+alpha = 0.05
+mca = MCA(predictor_preprocessed, predictand_preprocessed, nm, alpha)
+mca.save('mca_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+# mca = MCA.load('mca_', 'data-Pacific_Impact_Sahelian_Rainfall/', dsy=predictor_preprocessed, dsz=predictand_preprocessed)
+
+# Third step. Crossvalidation: skill and hidcast evaluation and products
+cross = Crossvalidation(predictor_preprocessed, predictand_preprocessed, nm, alpha)
+cross.save('cross_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+# cross = Crossvalidation.load('cross_', 'data-Pacific_Impact_Sahelian_Rainfall/', dsy=predictor_preprocessed, dsz=predictand_preprocessed)
+
+mca.plot(save_fig=True, cmap='viridis', name='mca.png', folder='plots-Pacific_Impact_Sahelian_Rainfall/', suy_ticks=[-0.25, -0.125, 0, 0.125, 0.250], suz_ticks=[-0.25, -0.125, 0, 0.125, 0.250])
+cross.plot(save_fig=True, cmap='viridis', name='cross.png', folder='plots-Pacific_Impact_Sahelian_Rainfall/')
---
+++ README.md
@@ -0,0 +1,33 @@
+# Spy4CastManual
+
+Spy4Cast is an open-source python implementation of a 
+maximum convariance analysis (MCA) statistical model.
+
+In this manual we explain the steps to take to install 
+and run for the first time this model.
+
+Full documentation is available at https://spy4cast-docs.netlify.app
+
+## Installation
+
+You have to have installed git (Install Git https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+
+You have to have installed anaconda (Install Anaconda https://docs.anaconda.com/anaconda/install/index.html)
+
+```bash
+    $ conda create -n <your-env-name> python=3.9
+    $ conda activate <your-env-name>
+    (<your-env-name>) $ conda install pip
+    (<your-env-name>) $ conda install cartopy
+    (<your-env-name>) $ pip install git+https://github.com/pabloduran016/Spy4Cast
+```
+
+Cartopy has to be installed with conda because pip version does not work
+
+## Usage
+
+For usage example look at file [EquatorialAtlantic_Impact_Nino.py](EquatorialAtlantic_Impact_Nino.py) in this repository.
+
+## How to cite this material
+
+DOI: https://10.5281/zenodo.8162066
