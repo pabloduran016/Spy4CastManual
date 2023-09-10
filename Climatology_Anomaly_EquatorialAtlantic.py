5819ddd30416a6aece800c04fbeaf3393bfeca8f
pabloduran <pdrm56@gmail.com>
2023-09-10 19:17:13 +0200

    New verison

--- Climatology_Anomaly_EquatorialAtlantic.py
+++ Climatology_Anomaly_EquatorialAtlantic.py
@@ -1,8 +1,9 @@
+import numpy as np
 from spy4cast import Dataset, Region, Month
 from spy4cast.meteo import Clim, Anom
 
 
-dataset_folder = "datasets"  # Folder where the datasets are stored.
+dataset_folder = "./datasets"  # Path to the folder where the datasets are stored.
 dataset_filename = "HadISST_sst-1970_2020.nc"  # File format must be netcdf4.
 ds = Dataset(dataset_filename, folder=dataset_folder)
 # A chunks keyword argument can be provided in this step. This value will be
@@ -11,10 +12,10 @@ ds = Dataset(dataset_filename, folder=dataset_folder)
 ds.open("sst")  # Opens the dataset, stores variables.
 
 region = Region(
-    lat0=5, latf=45,
-    lon0=-90, lonf=-5,
-    month0=Month.JUN, monthf=Month.JUL,
-    year0=1997, yearf=2019,
+    lat0=-30, latf=30,
+    lon0=-60, lonf=15,
+    month0=Month.MAY, monthf=Month.JUL,
+    year0=1976, yearf=2000
 )  # months can also be stated through integers.
 ds.slice(region)  # year0 and yearf apply to monthf.
 # ds.slice(region, skip=1)  # skip 1 data point in lat and lon dimension.
@@ -26,12 +27,22 @@ clim_ts = Clim(ds, "ts")  # Mean in the lat and lon dimension.
 anom_map = Anom(ds, "map")  # An anomaly map for each year
 anom_ts = Anom(ds, "ts")  # Mean in the lat and lon dimension
 # Plot with the .plot method (look at docs). Example:
-clim_map.plot(show_plot=True, save_fig=True, name="plots-Climatology_Anomaly_EquatorialAtlantic/clim_map.png")
-anom_map.plot(year=1999, show_plot=True, save_fig=True, name="plots-Climatology_Anomaly_EquatorialAtlantic/anom_map.png")
+clim_map.plot(
+    show_plot=True, save_fig=True,
+    name="plots-Climatology_Anomaly_EquatorialAtlantic/clim_map.png",
+    levels=np.arange(22, 28, 0.1),
+    ticks=np.arange(22, 28.5, 0.5),
+)
+anom_map.plot(
+    year=1997, show_plot=True, save_fig=True,
+    name="plots-Climatology_Anomaly_EquatorialAtlantic/anom_map.png",
+    levels=np.arange(-0.6, 0.6 + 0.05, 0.05),
+    ticks=np.arange(-0.6, 0.8, 0.2),
+)
 # Save the data with the .save method (look at docs). Example:
-anom_map.save("anom_map_", folder="data-Climatology_Anomaly_EquatorialAtlantic/")
+anom_map.save("anom_map_", folder="./data-Climatology_Anomaly_EquatorialAtlantic/")
 # Load previously saved data with the .load method (look at docs). Example:
-anom_map = Anom.load("anom_map_", folder="data-Climatology_Anomaly_EquatorialAtlantic/", type="map")
+anom_map = Anom.load("anom_map_", folder="./data-Climatology_Anomaly_EquatorialAtlantic/", type="map")
 
 # to show plots if show_plot=False:
 # # clim_ts.plot()
--- Pacific_Impact_Sahelian_Rainfall.py
+++ Pacific_Impact_Sahelian_Rainfall.py
@@ -1,45 +1,55 @@
+import numpy as np
 from spy4cast import Dataset, Region, Month
 from spy4cast.spy4cast import Preprocess, MCA, Crossvalidation
 
 # Pacific impact on Sahelian rainfall
 
-predictor = Dataset('HadISST_sst-1970_2020.nc', 'datasets').open('sst').slice(
+predictor = Dataset("HadISST_sst-1970_2020.nc", "./datasets").open("sst").slice(
     Region(lat0=-30, latf=30,
            lon0=-200, lonf=-60,
-           month0=Month.APR, monthf=Month.JUN,
+           month0=Month.MAY, monthf=Month.JUL,
            year0=1976, yearf=2000),
 )
 
-predictand = Dataset('cru_ts4.06.1950.2010.nc', 'datasets').open('pre').slice(
+predictand = Dataset("cru_ts4.06.1950.2010.nc", "./datasets").open("pre").slice(
     Region(lat0=0, latf=25,
            lon0=-20, lonf=20,
-           month0=Month.JUL, monthf=Month.SEP,
+           month0=Month.AUG, monthf=Month.SEP,
            year0=1976, yearf=2000),
 )
-#  There is a lag of 3 months (from April to July)
+#  There is a lag of 3 months (from May to August)
 
 ## METHODOLOGY
 
 # First step. Preprocess variables: anomaly and reshaping
 predictor_preprocessed = Preprocess(predictor)
-predictor_preprocessed.save('y_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+predictor_preprocessed.save("y_", "./data-Pacific_Impact_Sahelian_Rainfall/")
 # Save matrices as .npy for fast loading. To load use:
-# predictor_preprocessed = Preprocess.load('y_', 'data-Pacific_Impact_Sahelian_Rainfall/')
+# predictor_preprocessed = Preprocess.load("y_", "./data-Pacific_Impact_Sahelian_Rainfall/")
 predictand_preprocessed = Preprocess(predictand)
-predictand_preprocessed.save('z_', 'data-Pacific_Impact_Sahelian_Rainfall/')
-# predictand_preprocessed = Preprocess.load('z_', 'data/-Pacific_Impact_Sahelian_Rainfall')
+predictand_preprocessed.save("z_", "./data-Pacific_Impact_Sahelian_Rainfall/")
+# predictand_preprocessed = Preprocess.load("z_", "./data/-Pacific_Impact_Sahelian_Rainfall")
 
 # Second step. MCA: expansion coefficients and correlation and regression maps
 nm = 3
 alpha = 0.05
 mca = MCA(predictor_preprocessed, predictand_preprocessed, nm, alpha)
-mca.save('mca_', 'data-Pacific_Impact_Sahelian_Rainfall/')
-# mca = MCA.load('mca_', 'data-Pacific_Impact_Sahelian_Rainfall/', dsy=predictor_preprocessed, dsz=predictand_preprocessed)
+mca.save("mca_", "./data-Pacific_Impact_Sahelian_Rainfall/")
+# mca = MCA.load("mca_", "./data-Pacific_Impact_Sahelian_Rainfall/", dsy=predictor_preprocessed, dsz=predictand_preprocessed)
 
 # Third step. Crossvalidation: skill and hidcast evaluation and products
 cross = Crossvalidation(predictor_preprocessed, predictand_preprocessed, nm, alpha)
-cross.save('cross_', 'data-Pacific_Impact_Sahelian_Rainfall/')
-# cross = Crossvalidation.load('cross_', 'data-Pacific_Impact_Sahelian_Rainfall/', dsy=predictor_preprocessed, dsz=predictand_preprocessed)
+cross.save("cross_", "./data-Pacific_Impact_Sahelian_Rainfall/")
+# cross = Crossvalidation.load("cross_", "./data-Pacific_Impact_Sahelian_Rainfall/", dsy=predictor_preprocessed, dsz=predictand_preprocessed)
 
-mca.plot(save_fig=True, cmap='viridis', name='mca.png', folder='plots-Pacific_Impact_Sahelian_Rainfall/', suy_ticks=[-0.25, -0.125, 0, 0.125, 0.250], suz_ticks=[-0.25, -0.125, 0, 0.125, 0.250])
-cross.plot(save_fig=True, cmap='viridis', name='cross.png', folder='plots-Pacific_Impact_Sahelian_Rainfall/')
+mca.plot(
+    save_fig=True, cmap="viridis", name="mca.png",
+    folder="./plots-Pacific_Impact_Sahelian_Rainfall/",
+    suy_ticks=[-0.25, -0.125, 0, 0.125, 0.250],
+    suz_ticks=np.arange(-9, 11, 2))
+cross.plot(
+    save_fig=True, cmap="viridis", name="cross.png",
+    folder="./plots-Pacific_Impact_Sahelian_Rainfall/",
+    map_ticks=[-0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2],
+    map_levels=[-0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2],
+)
