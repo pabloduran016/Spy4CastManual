bc3b884550fd733f707465b8d541a53207098b1f
pabloduran <pdrm56@gmail.com>
2024-04-02 19:46:50 +0200

    Update regions and add validation example

--- Pacific_Impact_Sahelian_Rainfall.py
+++ Pacific_Impact_Sahelian_Rainfall.py
@@ -7,22 +7,22 @@ from spy4cast.spy4cast import Preprocess, MCA, Crossvalidation
 predictor = Dataset("HadISST_sst-1970_2020.nc", "./datasets").open("sst").slice(
     Region(lat0=-30, latf=30,
            lon0=-200, lonf=-60,
-           month0=Month.MAY, monthf=Month.JUL,
-           year0=1976, yearf=2000),
+           month0=Month.APR, monthf=Month.JUN,
+           year0=1976, yearf=2009),
 )
 
 predictand = Dataset("cru_ts4.06.1950.2010.nc", "./datasets").open("pre").slice(
     Region(lat0=0, latf=25,
            lon0=-20, lonf=20,
-           month0=Month.AUG, monthf=Month.SEP,
-           year0=1976, yearf=2000),
+           month0=Month.JUL, monthf=Month.SEP,
+           year0=1976, yearf=2009),
 )
-#  There is a lag of 3 months (from May to August)
+#  There is a lag of 3 months (from April to July)
 
 ## METHODOLOGY
 
 # First step. Preprocess variables: anomaly and reshaping
-predictor_preprocessed = Preprocess(predictor)
+predictor_preprocessed = Preprocess(predictor, period=4, order=4)
 predictor_preprocessed.save("y_", "./data-Pacific_Impact_Sahelian_Rainfall/")
 # Save matrices as .npy for fast loading. To load use:
 # predictor_preprocessed = Preprocess.load("y_", "./data-Pacific_Impact_Sahelian_Rainfall/")
@@ -45,8 +45,8 @@ cross.save("cross_", "./data-Pacific_Impact_Sahelian_Rainfall/")
 mca.plot(
     save_fig=True, cmap="viridis", name="mca.png",
     folder="./plots-Pacific_Impact_Sahelian_Rainfall/",
-    suy_ticks=[-0.25, -0.125, 0, 0.125, 0.250],
-    suz_ticks=np.arange(-9, 11, 2))
+    ruy_ticks=[-0.25, -0.125, 0, 0.125, 0.250],
+    ruz_ticks=np.arange(-9, 11, 2))
 cross.plot(
     save_fig=True, cmap="viridis", name="cross.png",
     folder="./plots-Pacific_Impact_Sahelian_Rainfall/",
